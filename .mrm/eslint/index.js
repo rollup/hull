7110fa9b853630631fc4fd6636502b84c142f916
shellscape <andrew@shellscape.org>
2018-07-18 11:58:31 -0400

    fix: add snapshot files to eslintignore

--- .mrm/eslint/index.js
+++ .mrm/eslint/index.js
@@ -2,7 +2,6 @@ const merge = require('merge-options').bind({ concatArrays: true });
 const { json, lines } = require('mrm-core');
 
 module.exports = () => {
-
   const configs = [
     './config/base',
     './config/best-practices',
@@ -17,10 +16,9 @@ module.exports = () => {
   // eslint-disable-next-line import/no-dynamic-require, global-require
   const eslintrc = configs.reduce((result, config) => merge(result, require(config)), {});
 
-  json('.eslintrc.json', eslintrc)
-    .save();
+  json('.eslintrc.json', eslintrc).save();
 
   lines('.eslintignore')
-    .add(['/node_modules', '/dist'])
+    .add(['/node_modules', '/dist', '*.snap'])
     .save();
 };
